32a6a71d421a61ad1a28857598ae9ffbf514c4f2
Kaushal Patel <kaushalpatel1512@gmail.com>
2023-08-11 03:30:25 +0530

    initial commit after creating repo..

--- Hotel-Microservices/UserService/src/main/java/com/userservice/Client/HotelService.java
+++ Hotel-Microservices/UserService/src/main/java/com/userservice/Client/HotelService.java
@@ -3,7 +3,8 @@ import com.userservice.Model.Hotel;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
+//this is the syncronous microservice..
+//where hotel mciroservice can accessable by user microservices.
 @FeignClient(name="HOTEL-SERVICE")
 public interface HotelService {
 
